afef431dbec438d02ed74943e73ff8a8a3f2d72b
Apple <apple@Apples-MacBook-Pro.local>
2019-06-07 15:32:59 -0500

    iteration 2, unfinished

--- Emojinal/ViewController.swift
+++ Emojinal/ViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
+    let emoijinal_emojis = ["😁": "happy", "🌋": "lava you" ]
+    
     @IBAction func showMessage(_ sender: UIButton) {
         var alertController = UIAlertController(title: "Happy", message: "You're Awesome!", preferredStyle: UIAlertController.Style.alert)
         
@@ -21,33 +22,32 @@ class ViewController: UIViewController {
     }
     
         
-    @IBAction func showMessageSad(_ sender: UIButton) {
-        var alertController = UIAlertController(title: "Sad", message: "Everything will be ok!", preferredStyle: UIAlertController.Style.alert)
-        
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-        
-        present(alertController, animated: true, completion: nil)
-        
-    }
-    
-    @IBAction func stress(_ sender: UIButton) {
-        var alertController = UIAlertController(title: "Stress", message: "Take a deep breath", preferredStyle: UIAlertController.Style.alert)
-        
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-        
-        present(alertController, animated: true, completion: nil)
-
-        
-    }
-    
-    @IBAction func bread(_ sender: UIButton) {
-        var alertController = UIAlertController(title: "Confidence (or Hungry?)", message: "Let's get this bread (bagette)!", preferredStyle: UIAlertController.Style.alert)
-        
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-        
-        present(alertController, animated: true, completion: nil)
-    }
-    
-    
+//    @IBAction func showMessageSad(_ sender: UIButton) {
+//        var alertController = UIAlertController(title: "Sad", message: "Everything will be ok!", preferredStyle: UIAlertController.Style.alert)
+//
+//        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+//
+//        present(alertController, animated: true, completion: nil)
+//
+//    }
+//
+//    @IBAction func stress(_ sender: UIButton) {
+//        var alertController = UIAlertController(title: "Stress", message: "Take a deep breath", preferredStyle: UIAlertController.Style.alert)
+//
+//        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+//
+//        present(alertController, animated: true, completion: nil)
+//
+//
+//    }
+//
+//    @IBAction func bread(_ sender: UIButton) {
+//        var alertController = UIAlertController(title: "Confidence (or Hungry?)", message: "Let's get this bread (bagette)!", preferredStyle: UIAlertController.Style.alert)
+//
+//        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+//
+//        present(alertController, animated: true, completion: nil)
+//    }
+//
+//
 }
-
